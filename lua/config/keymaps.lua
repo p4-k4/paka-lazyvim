28d56cde9626f12202700f41b7008f4e469f988e
Paka <paka@Pakas-Mac-mini.lan>
2024-02-04 04:40:29 +1300

    sync

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+map("n", "<leader>fs", ":w<cr>", { desc = "Write" })
+
+-- Plugins - Focus
+map("n", "<leader>wj", "<cmd>FocusSplitDown<cr>", { desc = "FocusSplitDown" })
+map("n", "<leader>wk", "<cmd>FocusSplitUp<cr>", { desc = "FocusSplitUp" })
+map("n", "<leader>wh", "<cmd>FocusSplitLeft<cr>", { desc = "FocusSplitLeft" })
+map("n", "<leader>wl", "<cmd>FocusSplitRight<cr>", { desc = "FocusSplitRight" })
--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,4 +1,4 @@
 return {
   "folke/trouble.nvim",
-  opts = { position = "top" },
+  opts = { position = "bottom" },
 }
